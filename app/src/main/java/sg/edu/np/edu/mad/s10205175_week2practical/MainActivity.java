51473b6eb22121078259216eb74a68c7900251a2
Zafeerah15 <zafeerah15@gmail.com>
2022-05-07 01:34:26 +0800

    practical 2

--- app/src/main/java/sg/edu/np/edu/mad/s10205175_week2practical/MainActivity.java
+++ app/src/main/java/sg/edu/np/edu/mad/s10205175_week2practical/MainActivity.java
@@ -13,8 +13,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void follow (View view){
-        TextView txt = findViewById(R.id.button2);
-        txt.setText("welcome to App");
+    public void onClick(View view) {
+        if(user1.followed == false)
+        {
+            user1.followed = true;
+        }
+        else
+        {
+            user1.followed = false;
+        }
+        followstatus(user1,followbutton);
     }
-}
+};
+public class userclass initial(
+        {
+        userclass initialization = new userclass("name","description",0,false);
+        return initialization;
+        };
+
+public void followstatus (User userclass, Button button)
+        {
+        TextView txt = button;
+        if(user.followed == false)
+        {
+        txt.setText("Follow");
+        }
+        else
+        {
+        txt.setText("Unfollow");
+        }
+        }
+
+
+
+
